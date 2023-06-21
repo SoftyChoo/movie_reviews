83a178ff86f52acae3803d0576f6c9a554af8bd3
추민수 <devsoftychoo@gmail.com>
2023-06-21 15:44:16 +0900

    new

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomePage() // 홈페이지 보여주기
+        );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 음식 사진 데이터
+    List<Map<String, dynamic>> dataList = [
+      {
+        "category": "탑건: 매버릭",
+        "imgUrl": "https://i.ibb.co/sR32PN3/topgun.jpg",
+      },
+      {
+        "category": "마녀2",
+        "imgUrl": "https://i.ibb.co/CKMrv91/The-Witch.jpg",
+      },
+      {
+        "category": "범죄도시2",
+        "imgUrl": "https://i.ibb.co/2czdVdm/The-Outlaws.jpg",
+      },
+      {
+        "category": "헤어질 결심",
+        "imgUrl": "https://i.ibb.co/gM394CV/Decision-to-Leave.jpg",
+      },
+      {
+        "category": "브로커",
+        "imgUrl": "https://i.ibb.co/MSy1XNB/broker.jpg",
+      },
+      {
+        "category": "문폴",
+        "imgUrl": "https://i.ibb.co/4JYHHtc/Moonfall.jpg",
+      },
+    ];
+
+    // 화면에 보이는 영역
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        title: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "Movie Reviews",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            color: Colors.black,
+            icon: Icon(Icons.person_outline),
+            onPressed: () {
+              // 아이콘을 클릭했을 때 수행할 동작을 추가할 수 있습니다.
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "영화 제목을 검색해주세요.",
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {},
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Divider(
+            height: 1,
+            color: Colors.white,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                String category = dataList[index]["category"];
+                String imgUrl = dataList[index]["imgUrl"];
+
+                return Card(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.5), BlendMode.darken),
+                        child: Image.network(
+                          imgUrl,
+                          width: double.infinity,
+                          height: 220,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Text(
+                        category,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
